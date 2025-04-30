ebd1e9e2e4cde2596ca7985a153d95f43c0fd8b0
Wrub0002 <161263880+Wrub0002@users.noreply.github.com>
2025-04-30 18:45:11 -0400

    add fies via upload
    Initial commit - learning JavaScript basics

---
+++ index.js
@@ -0,0 +1,12 @@
+//console.log("Hello, world!");
+//console.log("This is a test.");
+
+//window.alert("Hello, world!");
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like Pizza!"
+//this is a comment
+
+/* 
+this is a comment 
+*/
