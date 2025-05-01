e3a88b7ac6c1cffb3f019ce64eb64c921fcb108a
Wrub0002 <Wrub0002@algonquinlive.com>
2025-05-01 18:16:02 -0400

    Learning Variables

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-//console.log("Hello, world!");
-//console.log("This is a test.");
+/* === Introduction ===
 
-//window.alert("Hello, world!");
+console.log("Hello, world!");
+console.log("This is a test.");
+
+window.alert("Hello, world!");
 
 document.getElementById("myH1").textContent = "Hello";
 document.getElementById("myP").textContent = "I like Pizza!"
+
 //this is a comment
 
-/* 
-this is a comment 
 */
+
+/* === Variables === */
+let x;
+
+x = 123;
+console.log(x)
+
+// let age = 21;
+console.log(`You are ${age} years old`);
+console.log(typeof age);
+
+let firstname = "Leonardo";
+console.log(`Your first name is ${firstname}`)
+console.log(typeof firstname);
+
+let online = true;
+console.log(`${firstname} is online: ${online}`)
+console.log(typeof online)
+
+let fullname = "Leonardo Wrubleski"
+let age = 21;
+let student = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullname}`
+document.getElementById("p2").textContent = `Your are ${age} yeas old`
+document.getElementById("p3").textContent = `Enrolled: ${student}`
+
