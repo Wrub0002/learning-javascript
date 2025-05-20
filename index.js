aae5951ae1b1aea7460fc765f636b42e8cecb3b3
Wrub0002 <Wrub0002@algonquinlive.com>
2025-05-19 21:57:38 -0400

    variable scope

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
-/* === Function === 
+/* === variable scope === */
 
+let x = 3;
 
+function2();
 
-function happyBirthday(username, age){
-    
-    
-    console.log("Happy Birthday to you ");
-    console.log("Happy Birthday to you ");
-    console.log(`Happy Birthday dear ${username}`);
-    console.log("Happy Birthday to you ");
-    console.log(`You are ${age}`);
-
+function function1(){
+    let x = 1;
+    console.log(x);
 }
 
-happyBirthday("Leonardo", "21");
-happyBirthday("Maria", "19")
-*/
-
-function add(x,y){
-    let result = x + y;
-    return result;
+function function2(){
+    let x = 2;
+    console.log(x);
 }
-
-function subtract(x, y){
-    return x - y
-}
-function Multiply(x, y){
-    return x * y
-}
-
-console.log(add(1,2))
-console.log(subtract(3,2))
-console.log(Multiply(3,2))
