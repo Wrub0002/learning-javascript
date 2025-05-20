499caa03ca2c1993a1291d2406f82bbbb1368569
Wrub0002 <Wrub0002@algonquinlive.com>
2025-05-19 21:52:21 -0400

    Functions

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-/* === For loop ===  
+/* === Function === 
 
-for (let i = 10; i > 0; i--) {
-  console.log(i);
+
+
+function happyBirthday(username, age){
+    
+    
+    console.log("Happy Birthday to you ");
+    console.log("Happy Birthday to you ");
+    console.log(`Happy Birthday dear ${username}`);
+    console.log("Happy Birthday to you ");
+    console.log(`You are ${age}`);
 
 }
 
-console.log("HAPPY NEW YEAR!")
-
+happyBirthday("Leonardo", "21");
+happyBirthday("Maria", "19")
 */
 
-for (let couting = 20; couting > 0; couting--){
-
-    if(couting == 13){
-        continue;
-    }
-    else{
-        console.log(couting);
-    }
-
+function add(x,y){
+    let result = x + y;
+    return result;
 }
-console.log("HAPPY NEW YEAR!")
+
+function subtract(x, y){
+    return x - y
+}
+function Multiply(x, y){
+    return x * y
+}
+
+console.log(add(1,2))
+console.log(subtract(3,2))
+console.log(Multiply(3,2))
