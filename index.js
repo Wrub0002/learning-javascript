49a1d8cb93adcc8de9fdea2d0d9707d403cbcae5
Wrub0002 <Wrub0002@algonquinlive.com>
2025-05-24 18:20:38 -0400

    array

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-/* === variable scope === */
+/* ===  array  === 
 
-let x = 3;
+let fruits = ["apple", "orange", "banana"]
 
-function2();
+fruits[0] = "coconut"
+fruits.push = "pear"
+fruits.pop()
+fruits.unshift()
 
-function function1(){
-    let x = 1;
-    console.log(x);
-}
 
-function function2(){
-    let x = 2;
-    console.log(x);
-}
+console.log(fruits)
+console.log(fruits[0])
+console.log(fruits[1])
+*/
+
+let fruits = ["apple", "orange", "banana"]
+
+for (let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+ } 
